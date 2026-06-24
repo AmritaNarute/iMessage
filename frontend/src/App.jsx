1fcb495b3b954c2da15ae2836bc591ca282aabd1
AmrutaNarute <amruta.narute@dwebpixel.com>
2026-06-24 18:58:42 +0530

    added ui File and Packages as well

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,32 @@
 
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { WallpaperProvider } from "./context/wallpaperContext"
+import { ThemeProvider } from "./context/ThemeContext";
+import { Navigate, Route, Routes } from "react-router"
+import ChatPage from "./pages/ChatPage"
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return (
-    <div>
-      <h1>MY APP</h1>
 
-     <header>
-        <Show when="signed-out">
-          <SignInButton  model="modal" />
-          <SignUpButton />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+   const { isSignedIn, isLoaded } = useAuth();
+
+  //todo: make this a better component
+  if (!isLoaded) return <p>loading...</p>
+
+  return (
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route
+            path="/auth"
+            element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
+          />
+        </Routes>
+        <Toaster />
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/pages/ChatPage.jsx
@@ -0,0 +1,32 @@
+import { useWallpaper } from "../context/wallpaper";
+import { useEffect } from "react";
+
+function ChatPage() {
+  const { frameStyle } = useWallpaper();
+
+
+  useEffect(() => {
+    if (!activeConversationId) return;
+
+    getMessages(activeConversationId);
+    subscribeToMessages(activeConversationId);
+
+    // cleanup
+    return () => unsubscribeFromMessages();
+  }, [getMessages, activeConversationId, subscribeToMessages, unsubscribeFromMessages]);
+
+  return (
+    <div className="flex h-dvh flex-col overflow-hidden p-2 sm:p-3 md:p-8" style={frameStyle}>
+      <div className="mx-auto flex w-full max-w-6xl flex-1 overflow-hidden rounded-2xl border border-border bg-background text-foreground">
+        
+        <div
+          className={`flex-1 flex-col overflow-hidden ${
+            !isLargeScreen && !activeConversationId ? "hidden lg:flex" : "flex"
+          }`}
+        >
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ChatPage;
