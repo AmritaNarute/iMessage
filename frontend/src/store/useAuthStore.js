b2e7efd5b9556033cc4de79fcfb81162beb5edbf
AmrutaNarute <amruta.narute@dwebpixel.com>
2026-07-01 18:39:06 +0530

    renamed file name

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,6 @@
 import { create } from 'zustand';
 import { axiosInstance } from '../lib/axios';
 import { io } from "socket.io-client"
-import { disconnect } from 'cluster';
 
 const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
 
